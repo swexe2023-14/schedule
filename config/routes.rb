839d81ba9344984a6b415ba82d382dd5d0ca41f5
ogawayuki <2111140072d@ed.fuk.kindai.ac.jp>
2023-12-06 06:04:18 +0000

    確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'top/main'
-  post 'top/login'
-  get 'top/logout'
   get 'users/index'
   root 'users#index'
   resources :users
--- db/migrate/20231115085937_create_users.rb
+++ db/migrate/20231115085937_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :uid
-      @user = User.find(session[:user_id])
       t.string :pass
 
       t.timestamps
