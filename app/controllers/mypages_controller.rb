53f37627df04773e05d1b8862db0b75592cf1034
ogawayuki <2111140072d@ed.fuk.kindai.ac.jp>
2024-01-08 14:18:45 +0000

    所属グループ表示

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,7 +2,7 @@ class MypagesController < ApplicationController
     def index
       @user = User.find_by(uid: session[:login_uid])
       @groups = Group.all
+      @user_groups = @user.join_groups.order(:name)
     end
-    
     
 end
